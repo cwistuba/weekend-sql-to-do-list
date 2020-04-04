442372a72e92fd9c4664d3d3e5391b465dc5d6e5
Caleb Wistuba <calebwistuba@gmail.com>
2020-04-04 10:36:00 -0500

    client.js receives data from front end and sends to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,29 @@ $(document).ready(init);
 
 function init() {
   console.log("ToDo List Running");
+  $("#js-submit-doItem").on("submit", submitItem);
+}
+
+function submitItem(event) {
+  event.preventDefault();
+
+  const doItemInput = $("#js-input-doItem").val();
+
+  console.log(doItemInput);
+}
+
+function postDoItem(doItem) {
+  const dataForServer = {
+    doItem: doItem,
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "/todoitems",
+    data: dataForServer,
+  });
+}
+
+function clearItem() {
+  $("#js-input-doItem").val("");
 }
